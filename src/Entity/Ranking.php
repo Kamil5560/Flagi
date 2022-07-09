8d1911d3315593eead128f0b5b753f898059f40c
MSI <kamil5560@gmail.com>
2022-07-10 00:05:16 +0200

    08.07 - CRM

---
+++ src/Entity/Ranking.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RankingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RankingRepository::class)]
+class Ranking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'bigint')]
+    private $result;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $uploaded_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getResult(): ?string
+    {
+        return $this->result;
+    }
+
+    public function setResult(string $result): self
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
+    public function getUploadedAt(): ?\DateTimeImmutable
+    {
+        return $this->uploaded_at;
+    }
+
+    public function setUploadedAt(\DateTimeImmutable $uploaded_at): self
+    {
+        $this->uploaded_at = $uploaded_at;
+
+        return $this;
+    }
+}
